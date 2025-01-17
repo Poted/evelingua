173de826dba674a252ac637985bcd0d12884d818
siwek_piotr@ma-al.com <o.jpkmc@gmail.com>
2025-01-17 03:07:44 +0100

    feat: add colors

---
+++ backend/helpers/colors/colors.go
@@ -0,0 +1,36 @@
+package colors
+
+import (
+	"fmt"
+	"log"
+)
+
+var Reset Colors = "\033[0m"
+var Red Colors = "\033[31m"
+var Green Colors = "\033[32m"
+var Yellow Colors = "\033[33m"
+var Blue Colors = "\033[34m"
+var Magenta Colors = "\033[35m"
+var Cyan Colors = "\033[36m"
+var Gray Colors = "\033[37m"
+var White Colors = "\033[97m"
+
+type Colors string
+
+func FuncInColors(color Colors, fn func()) {
+	fmt.Print(color)
+	fn()
+	fmt.Print(Reset)
+}
+
+func LogInColors(color Colors, msg string) {
+	fmt.Print(color)
+	log.Default().Print(msg)
+	fmt.Print(Reset)
+}
+
+func ErrInColors(err string) {
+	fmt.Print(Red)
+	log.Default().Print(err)
+	fmt.Print(Reset)
+}
